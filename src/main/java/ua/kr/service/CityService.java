b37704af035049174994bb7442984023f3861239
Oleh <oleh.kepsha@mycit.ie>
2019-12-19 23:31:16 +0000

    playable version

--- src/main/java/ua/kr/service/CityService.java
+++ src/main/java/ua/kr/service/CityService.java
@@ -1,6 +1,7 @@
 package ua.kr.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ThreadLocalRandom;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,41 +11,60 @@ import ua.kr.component.CityImportComponent;
 
 @Service
 public class CityService {
-    @Autowired
-    private CityImportComponent cityImport;
+	@Autowired
+	private CityImportComponent cityImport;
 
-    private List <String> cities;
-    private String currentCity;
-    private char lastChar;
+	private List <String> cities;
+	private String currentCity;
+	private char lastChar;
 
-    public void startGame() throws Exception {
-        cities = cityImport.importCity();
-        currentCity = cities.get(ThreadLocalRandom.current().nextInt(cities.size()));
-        lastChar = currentCity.charAt(currentCity.length() - 1);
-        check();
-        prompt();
+	public void startGame() throws Exception {
+		cities = cityImport.importCity();
+		currentCity = cities.get(ThreadLocalRandom.current().nextInt(cities.size()));
+		lastChar = currentCity.charAt(currentCity.length() - 1);
+		check();
+		prompt();
 
-    }
+	}
 
-    public void answer( String city ) {
-        if (Character.toLowerCase(city.charAt(0)) == lastChar) {
-            if (cities.contains(city)) {
-                currentCity = city;
-                lastChar = currentCity.charAt(currentCity.length() - 1);
-                check();
-                prompt();
-            }
-        }
-    }
+	public void answer( String city ) {
+		if (Character.toLowerCase(city.charAt(0)) == lastChar) {
+			if (cities.contains(city)) {
+				currentCity = city;
+				lastChar = currentCity.charAt(currentCity.length() - 1);
+				check();
 
-    private void prompt() {
-        System.out.println(currentCity);
-        System.out.println("Тебе на " + lastChar);
-    }
+				//prompt();
 
-    private void check() {
-        if (lastChar == 'ы' || lastChar == 'ь') {
-            lastChar = currentCity.charAt(currentCity.length() - 2);
-        }
-    }
+				cities.remove(currentCity);
+				
+				Optional<String> opt = cities.stream().filter(cit -> 
+					Character.toLowerCase(cit.charAt(0)) == lastChar
+				).findFirst();
+				
+				if(opt.isPresent()) {
+					currentCity = opt.get();
+					lastChar = currentCity.charAt(currentCity.length() - 1);
+					check();
+					prompt();
+					cities.remove(currentCity);
+				} else {
+					System.out.println("You Won!!!");
+				}
+			}
+
+		}
+	}
+
+
+	private void prompt() {
+		System.out.println(currentCity);
+		System.out.println("Тебе на " + lastChar);
+	}
+
+	private void check() {
+		if (lastChar == 'ы' || lastChar == 'ь') {
+			lastChar = currentCity.charAt(currentCity.length() - 2);
+		}
+	}
 }
